daf88d58d24de08c852ecbfee127526b5f959dec
Sean Manhertz <sean.manhertz@gmail.com>
2014-04-08 17:18:31 -0600

    Original assignment done, trying to implement objects

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ while (isNaN(victims) === true || victims === 0) {
    alert("The input cannot be parsed to a number");
    var victims = prompt("How many victims have you found?","# of victims")
    }
+function victim (victimsname, victimsphone, victimsaddress) {
+	this.victimsname= victimsname;
+	this.victimsphone= victimsphone;
+	this.victimsaddress= victimsaddress;
+}
+var victimstoask= victims;
+while (victimstoask-- ){
+	this.victimsname= victimsname=prompt("Please enter their name","");
+	this.victimsphone= victimsphone=prompt("Please enter their phone number");
+	this.victimsaddress=victimsaddress=prompt("Please enter their street address");
+	var random = new victim (victimsname,victimsphone,victimsaddress);
+}
    // if (victims === 0)
    //    {
    //    alert("The number is zero. Please re-enter the number you victims you wish to help today");
@@ -19,16 +31,33 @@ while (isNaN(victims) === true || victims === 0) {
 // 	  return false;
 //    }
 
-var victimstoask= victims;
-	victimsname=[];
-	victimsphone=[];
-	victimsaddress=[];
-while (victimstoask-- ) {
-		victimsname.push( name=prompt("Please enter their name",""));
-		victimsphone.push( phone=prompt("Please enter their phone number"));
-		victimsaddress.push( address=prompt("Please enter their street address'"));
-}
-alert(victimsname.length+" are in need of help")
+// var victimstoask= victims;
+// 	victimsname=[];
+// 	victimsphone=[];
+// 	victimsaddress=[];
+// while (victimstoask-- ) {
+// 		victimsname.push( name=prompt("Please enter their name",""));
+// 		victimsphone.push( phone=prompt("Please enter their phone number"));
+// 		victimsaddress.push( address=prompt("Please enter their street address"));
+// }
+// var volunteers = prompt("How many volunteers are available?","# of volunteers")
+// var volunteers = parseInt(volunteers);
+// while (isNaN(volunteers) === true || volunteers === 0) {
+//    alert("The input cannot be parsed to a number");
+//    var volunteers = prompt("How many volunteers are available?","# of volunteers")
+//    }
+//  var volunteerstoask= volunteers;
+// 	volunteersname=[];
+// 	volunteersphone=[];
+// 	volunteersaddress=[];
+// while (volunteerstoask-- ) {
+// 		volunteersname.push( name=prompt("Please enter their name",""));
+// 		volunteersphone.push( phone=prompt("Please enter their phone number"));
+// 		volunteersaddress.push( address=prompt("Please enter their street address"));
+// }  
+// var displayvictims= victimsname.join(", ")
+// var displayvolunteers= volunteersname.join(", ")
+// alert(victims+" people are in need of help. We have "+volunteers+" volunteers that can help out. The victims that need help are "+displayvictims+". The volunteers that can help out are "+displayvolunteers)
 
 // victimsinfo.sort( byName );
 // document.getElementById( 'msg' ).textContent = victimsinfo.join(',');
